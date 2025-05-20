7358bef14c650d106d2c7ea5c88968b71078ffad
David Seybold <davidseybold@pm.me>
2025-05-20 15:52:48 -0400

    Fix down migration

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS change_targets;
-
 DROP TABLE IF EXISTS changes;
 
 DROP TABLE IF EXISTS resource_records;
@@ -7,5 +5,3 @@ DROP TABLE IF EXISTS resource_records;
 DROP TABLE IF EXISTS resource_record_sets;
 
 DROP TABLE IF EXISTS zones;
-
-DROP TABLE IF EXISTS servers;
